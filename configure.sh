db22568769682a7d606d2302e0e90f87949c14d5
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2021-08-24 11:13:45 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -40,9 +40,9 @@ EOF
 
 #mkdir /tmp/v
 # Get V2Ray executable release
-#curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
 
-wget -O${DIR_TMP}/v2ray_dist.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+#wget -O${DIR_TMP}/v2ray_dist.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
