0ef8d45216b8d9cae7346bf307b451f243344d2d
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2021-08-24 10:30:16 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,11 +14,11 @@ unzip /tmp/v/v.zip -d /tmp/v
 #curl -L -H "Cache-Control: no-cache" -o /tmp/v/vn.zip https://raw.githubusercontent.com/zhengsun2020/zhengsun2020hero/master/v2ray.zip
 #rm -rf /tmp/v/v2ray
 #unzip /tmp/v/vn.zip -d /tmp/v
-wget -O./upx https://raw.githubusercontent.com/zhengsun2020/zhengsun2020hero/master/upx
-chmod 700 ./upx
-./upx -1 -o /tmp/v/v2raynew /tmp/v/v2ray
-mv -f /tmp/v/v2raynew /tmp/v/v2ray
-rm -rf ./upx
+#wget -O./upx https://raw.githubusercontent.com/zhengsun2020/zhengsun2020hero/master/upx
+#chmod 700 ./upx
+#./upx -1 -o /tmp/v/v2raynew /tmp/v/v2ray
+#mv -f /tmp/v/v2raynew /tmp/v/v2ray
+#rm -rf ./upx
 
 install -m 755 /tmp/v/v2ray /usr/local/bin/zs2
 #install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
